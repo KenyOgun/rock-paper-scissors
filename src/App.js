912e491fa3a3fa1f2cec4b82df3dfed95fd73693
KenyOgun <holleraturboi@yahoo.ca>
2022-11-07 23:49:22 -0500

    implementing the structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import { Title } from "./components/Title";
+import { Round } from "./components/Round";
+import { Playground } from "./components/Playground";
+import { Profile } from "./components/Profile";
+import { User } from "./components/User";
+import { Choice } from "./components/Choice";
+import { Computer } from "./components/Computer";
+import { Score } from "./components/Score";
+import { Message } from "./components/Message";
+import { Reset } from "./components/Reset";
+
 import { settings } from "./configs/game";
 import React, { useState } from "react";
 import rock from "./assets/rock.png";
@@ -8,17 +19,38 @@ import trophy from "./assets/trophy.png";
 import "./styles.css";
 
 export default function App() {
-  let [game, setGame] = useState({
-    userSelection: "",
-    pcSelection: "",
-    round: 0,
-    userScore: 0,
-    pcScore: 0,
-    message: "",
-  });
+  //state object
   return (
     <div className="App">
-      <p>Rock Paper Scissors Game</p>
+      <Title />
+      <Round {...game} />
+      <Playground>
+        <Profile>
+          <User {...game} trophyIcon={trophy}>
+            <Choice {...game} value="Rock" onClick={play} choiceIcon={rock} />
+            <Choice {...game} value="Paper" onClick={play} choiceIcon={paper} />
+            <Choice
+              {...game}
+              value="Scissors"
+              onClick={play}
+              choiceIcon={scissors}
+            />
+          </User>
+          <Score score={userScore} />
+        </Profile>
+        <Message {...game} />
+        <Profile>
+          <Computer
+            {...game}
+            rockIcon={rock}
+            paperIcon={paper}
+            scissorsIcon={scissors}
+            trophyIcon={trophy}
+          />
+          <Score score={pcScore} />
+        </Profile>
+      </Playground>
+      <Reset {...game} onClick={reset} />
     </div>
   );
 }
